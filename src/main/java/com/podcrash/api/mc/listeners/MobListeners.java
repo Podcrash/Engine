208386a5e05b5f7069a3b66edd57a989bf16a1a4
Flaymed <34602265+Flaymed@users.noreply.github.com>
2020-02-06 21:13:30 -0500

    Fixed NPE possibilities

--- src/main/java/com/podcrash/api/mc/listeners/MobListeners.java
+++ src/main/java/com/podcrash/api/mc/listeners/MobListeners.java
@@ -16,46 +16,59 @@ import com.podcrash.api.mc.mob.MobManager.mobs;
 import com.podcrash.api.mc.mob.MobData;
 
 public class MobListeners extends ListenerBase {
-  
 	@EventHandler(priority = EventPriority.LOW)
-	public void onEntityCombust(EntityCombustEvent event){
-		MobData mob = MobManager.getMobData(event.getEntity().getEntityId());
-		if (!mob.canBurn()) {
-			event.setCancelled(true);
-		}
-	}
-   
-   @EventHandler(priority = EventPriority.LOW)
-    public void onMobDamage(EntityDamageEvent e) {
-
-    	int id = e.getEntity().getEntityId();
-			MobData mob = MobManager.getMobData(id);
-		
-			if (!mob.isDamageable()) {
-				e.setCancelled(true);
-			} else {
-				return;
-			}
-
-    }
-    
-    @EventHandler(priority = EventPriority.LOW)
     public void onPotionSplash(PotionSplashEvent e) {
         Collection<LivingEntity> entities = e.getAffectedEntities();
+        ThrownPotion effect = e.getPotion();
+
         for (LivingEntity entity: entities) {
 
             int id = entity.getEntityId();
-            MobData mob = MobManager.getMobData(id);
+            MobData mob = mobs.get(id);
 
-            if (!mob.takesPotionEffects()) {
-                Collection<PotionEffect> entityEffects = entity.getActivePotionEffects();
-                for (PotionEffect entityEffect : entityEffects) {
-                    entity.removePotionEffect(entityEffect.getType());
+            if (mob == null) {
+                continue;
+            } else {
+                if (!mob.takesPotionEffects()) {
+                    Collection<PotionEffect> entityEffects = entity.getActivePotionEffects();
+                    for (PotionEffect entityEffect : entityEffects) {
+                        entity.removePotionEffect(entityEffect.getType());
+                    }
                 }
+            }
+
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onEntityCombust(EntityCombustEvent e){
+        int id = e.getEntity().getEntityId();
+        MobData mob = mobs.get(id);
+
+        if (mob == null) {
+            return;
+        } else {
+            if (!mob.canBurn()) {
+                e.setCancelled(true);
             }
         }
 
     }
-   
-   
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onMobDamage(EntityDamageEvent e) {
+
+        int id = e.getEntity().getEntityId();
+        MobData mob = mobs.get(id);
+
+        if (mob == null) {
+            return;
+        } else {
+            if(!mob.isDamageable()) {
+                e.setCancelled(true);
+            } else {
+                return;
+            }
+        }
 }
