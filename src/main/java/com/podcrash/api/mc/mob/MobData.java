58b0b01adf28df1c182af0b7a8cd75562bb8660d
Flaymed <34602265+Flaymed@users.noreply.github.com>
2020-02-17 20:04:19 -0500

    updated method names

--- src/main/java/com/podcrash/api/mc/mob/MobData.java
+++ src/main/java/com/podcrash/api/mc/mob/MobData.java
@@ -9,12 +9,16 @@ public class MobData {
 	private int id;
 	private boolean damage;
 	private boolean frozen;
+	private boolean burns;
+	private boolean potionEffects;
 
-	public MobData(Entity entity, int creatureId, boolean takesDamage, boolean isFrozen) {
+	public MobData(Entity entity, int creatureId, boolean takesDamage, boolean isFrozen, boolean canBurn, boolean takesPotionEffects) {
 		this.creature = entity;
 		this.id = creatureId;
 		this.damage = takesDamage;
 		this.frozen = isFrozen;
+		this.burns = canBurn;
+		this.potionEffects = takesPotionEffects;
 	}
 
 	public Entity getEntity() {
@@ -25,12 +29,35 @@ public class MobData {
 		return this.id;
 	}
 
+	public boolean takesPotionEffects() {
+		return this.potionEffects;
+	}
 
-	public boolean getDamageable() {
+	public void togglePotionEffects() {
+		if (this.potionEffects) {
+			this.potionEffects = false;
+		} else {
+			this.potionEffects = true;
+		}
+	}
+
+	public void toggleBurn() {
+		if (this.burns) {
+			this.burns = false;
+		} else {
+			this.burns = true;
+		}
+	}
+
+	public boolean canBurn() {
+		return this.burns;
+	}
+
+	public boolean isDamageable() {
 		return this.damage;
 	}
 
-	public boolean getFrozen() {
+	public boolean isFrozen() {
 		return this.frozen;
 	}
 
@@ -42,8 +69,16 @@ public class MobData {
 		this.damage = dmg;
 	}
 	
+	public void setEntity(Entity en) {
+		this.creature = en;
+	}
+	
 	public void toggleFreeze(boolean froze) {
 		this.frozen = froze;
 	}
-}
+	
+	public void setId(int newId) {
+		this.id = newId;
+	}
 
+}
