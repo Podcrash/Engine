38dd0885b766b5d8e36fea372653c2fe38941806
jiayang <jchen59@stuy.edu>
2020-03-27 13:40:27 -0400

    added currency enum

---
+++ src/main/java/com/podcrash/api/mc/economy/Currency.java
@@ -0,0 +1,32 @@
+package com.podcrash.api.mc.economy;
+
+import org.bukkit.ChatColor;
+
+public enum Currency {
+
+    COIN("coins", ChatColor.GOLD),
+    //CRYSTAL("Crystals", ChatColor.LIGHT_PURPLE)
+    ;
+
+    private final String name;
+    private final ChatColor color;
+
+
+    Currency(String name, ChatColor color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    /**
+     *
+     * @return The name of the currency LOWERCASE
+     */
+    public String getName() {return name;}
+
+    /**
+     *
+     * @return The formatting for the number that accompanies name
+     */
+    public String getFormatting() {return "" + color + ChatColor.BOLD;}
+
+}
