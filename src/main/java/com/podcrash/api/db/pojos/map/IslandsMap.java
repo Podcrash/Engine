6156ebff545daf9c5e757ad3be8cf9011079a081
Flaymed <mvigg8@gmail.com>
2020-04-10 15:36:28 -0400

    IslandsMap

--- src/main/java/com/podcrash/api/db/pojos/map/IslandsMap.java
+++ src/main/java/com/podcrash/api/db/pojos/map/IslandsMap.java
@@ -1,30 +1,63 @@
 package com.podcrash.api.db.pojos.map;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class IslandsMap extends GameMap {
 
     private int waterDmg;
-    List<Location> bridgePoints;
+    //All points of sponge with pressure plate on top
+    private HashMap<Integer, Location> bridgePoints;
+    //Two matching
+    private HashMap<Location, Location> bridgeParts;
+
+    //Integer is the # for the piece and LinkedList contains Location, Block
+    private HashMap<Integer, LinkedList<Object>> bridgePieces;
+    private int bridgeLength;
 
     public IslandsMap() {
         super();
-        this.bridgePoints = new LinkedList<>();
+        this.bridgePoints = new HashMap<Integer, Location>();
+        this.bridgeParts = new HashMap<>();
+        this.bridgePieces = new HashMap<>();
     }
 
-    public void addBridgePoint(Location location) {
-        this.bridgePoints.add(location);
+    public void setBridgeLength(int length) {
+        this.bridgeLength = length;
     }
 
-    public void removeBridgePoint(Location location) {
-        this.bridgePoints.remove(location);
+    public int getBridgeLength() {
+        return bridgeLength;
     }
 
-    public List<Location> getBridgePoints() {
+    public void addBridgePoint(int bridgeNumber, Location location) {
+        this.bridgePoints.put(bridgeNumber, location);
+    }
+
+    public HashMap<Integer, Location> getBridgePoints() {
         return bridgePoints;
+    }
+
+    public void addBridgePart(Location piece1, Location piece2) {
+        this.bridgeParts.put(piece1, piece2);
+    }
+
+    public HashMap<Location, Location> getBridgeParts() {
+        return bridgeParts;
+    }
+
+    public void addBridgePiece(Integer number, LinkedList<Object> locations) {
+
+        bridgePieces.put(number, locations);
+    }
+
+
+    public HashMap<Integer, LinkedList<Object>> getBridgePieces() {
+        return bridgePieces;
     }
 
 
@@ -38,9 +71,7 @@ public class IslandsMap extends GameMap {
 
     public String toString() {
         final StringBuilder sb = new StringBuilder("IslandsMap{");
-        sb.append("launchPads=").append(getLaunchPads());
-        sb.append(", teleportPads=").append(getTeleportPads());
-        sb.append(", name='").append(getName()).append('\'');
+        sb.append("name='").append(getName()).append('\'');
         sb.append(", gamemode='").append(getGamemode()).append('\'');
         sb.append(", authors=").append(getAuthors());
         sb.append(", spawns=").append(getSpawns());
